981e773e652cf732a1b96e9d063f6dc4c78b311e
Alessandro Tacini <alessandro.tacini@fieb.edu.br>
2025-04-11 21:12:34 -0300

    Metodo buscarPorId() foi adicionado

--- src/main/java/com/belval/crudrest/controller/ProdutoController.java
+++ src/main/java/com/belval/crudrest/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package com.belval.crudrest.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +20,33 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
 	
+	/**
+	 * Retorna todos os produtos
+	 * @return
+	 */
 	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body(repository.findAll());
+	}
+	
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Object> buscarPorId(
+			@PathVariable(value = "id") Integer id) {
+		
+		Optional<Produto> produto = repository.findById(id);
+		
+		if (produto.isPresent()) {
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(produto.get());
+		}
+		
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body("Produto não encontrado!");
+				
 	}
 	
 	
@@ -41,5 +66,7 @@ public class ProdutoController {
 				.body(produto);
 		
 	}
+	
+	
 
 }
